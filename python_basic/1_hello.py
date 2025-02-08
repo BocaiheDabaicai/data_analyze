1e6e033e8e78001a3821af9d37d9e42ed19c065d
Sonder <108516200+BocaiheDabaicai@users.noreply.github.com>
2025-02-08 12:33:19 +0800

    节7

---
+++ python_basic/1_hello.py
@@ -0,0 +1,14 @@
+print("hello world")
+
+# 字符串拼接
+print("hello" + " world")
+# 单引号
+print('hello world')
+# 反斜杠表示特殊字符，\表示转义符
+print("hello\"world")
+# 换行
+print("hello\nworld")
+# 三引号跨行字符串,单引号、双引号都可以
+print("""人生的乐趣便是无尽的探索，
+并感知其中的意味，
+爱便在其中""")
